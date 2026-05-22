64a79e468c6504beb2a6b92f7917daaeb702b920
Abhishek <abhishek.lekkikoni@acelucid.com>
2026-05-22 15:41:24 +0530

    test

--- main.tf
+++ main.tf
@@ -8,22 +8,23 @@ resource "aws_s3_bucket" "mybucekt" {
   bucket = "demo-22-05-26"
 }
 
-resource "aws_instance" "myec2" {
-  ami = var.ami_id
-  instance_type = var.instance_type
+# resource "aws_instance" "myec2" {
+#   ami = var.ami_id
+#   instance_type = var.instance_type
 
-  tags = {
-    Name="terraform_Ec2"
-  }
-}
+#   tags = {
+#     Name="terraform_Ec2"
 
-resource "aws_dynamodb_table" "terraform-lock" {
-  name = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+#   }
+# }
 
-   attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "terraform-lock" {
+#   name = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key = "LockID"
+
+#    attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }

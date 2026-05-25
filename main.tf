6a94cc14ae50ec1bad3cd3e77fd5194d005d75f3
Abhishek <abhishek.lekkikoni@acelucid.com>
2026-05-25 13:21:30 +0530

    creating subnet

--- main.tf
+++ main.tf
@@ -43,3 +43,15 @@ resource "aws_instance" "myec2" {
 resource "aws_vpc" "myvpc" {
   cidr_block= var.cidr
 }
+
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    name="mysubnet"
+  }
+}
